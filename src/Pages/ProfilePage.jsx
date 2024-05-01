677fc633de204c97d90e5727292db78d482b6eec
sonu <srssoumyaranjansahu123@gmail.com>
2024-05-01 13:20:11 +0530

    vg

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -10,6 +10,7 @@ const ProfilePage = () => {
 
 
     return (
+        
         <div className='bg-gray-200  flex justify-center items-center h-screen'>
             <div className='w-[92%] h-[90%] flex flex-col gap-10 bg-white overflow-y-auto overflow-hidden custom-scrollbar rounded-lg shadow-md'>
                 <div className='w-full md:flex-row flex-col flex h-fit'>
